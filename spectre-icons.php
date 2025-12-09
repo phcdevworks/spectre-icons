b7a8cea93d7b30c002e8542dd336376a29f59646
Bradley J Potts <brad.potts@coastdigitalgroup.com>
2025-12-10 01:38:11 +0800

    Refactor code style and update plugin version
    Standardized code formatting by removing spaces in function calls and conditionals. Updated the plugin version in the header to 0.0.1. No functional changes were made.

--- spectre-icons.php
+++ spectre-icons.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
 
  * Plugin Name: Spectre Icons
  * Plugin URI: https://github.com/phcdevworks/spectre-icons
  * Description: Modular Spectre icon libraries for WordPress builders. Includes Elementor integration.
- * Version: 1.0.0
+ * Version: 0.0.1
  * Author: Spectre Plugins by PHCDevworks
  * Author URI: https://phcdevworks.com/
  * Requires at least: 6.0
@@ -18,18 +19,18 @@
  * @package SpectreIcons
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
 	exit;
 }
 
-define( 'SPECTRE_ICONS_FILE', __FILE__ );
-define( 'SPECTRE_ICONS_PATH', __DIR__ );
-define( 'SPECTRE_ICONS_URL', plugin_dir_url( __FILE__ ) );
-define( 'SPECTRE_ICONS_VERSION', '1.0.0' );
-define( 'SPECTRE_ICONS_MANIFEST_PATH', SPECTRE_ICONS_PATH . '/assets/manifests' );
-define( 'SPECTRE_ICONS_MANIFEST_URL', SPECTRE_ICONS_URL . 'assets/manifests/' );
+define('SPECTRE_ICONS_FILE', __FILE__);
+define('SPECTRE_ICONS_PATH', __DIR__);
+define('SPECTRE_ICONS_URL', plugin_dir_url(__FILE__));
+define('SPECTRE_ICONS_VERSION', '1.0.0');
+define('SPECTRE_ICONS_MANIFEST_PATH', SPECTRE_ICONS_PATH . '/assets/manifests');
+define('SPECTRE_ICONS_MANIFEST_URL', SPECTRE_ICONS_URL . 'assets/manifests/');
 
-if ( ! function_exists( 'spectre_icons_load_textdomain' ) ) :
+if (! function_exists('spectre_icons_load_textdomain')) :
 	/**
 	 * Load translations for WordPress.org compatibility.
 	 */
@@ -37,10 +38,10 @@ if ( ! function_exists( 'spectre_icons_load_textdomain' ) ) :
 		load_plugin_textdomain(
 			'spectre-icons',
 			false,
-			dirname( plugin_basename( SPECTRE_ICONS_FILE ) ) . '/languages/'
+			dirname(plugin_basename(SPECTRE_ICONS_FILE)) . '/languages/'
 		);
 	}
-	add_action( 'init', 'spectre_icons_load_textdomain' );
+	add_action('init', 'spectre_icons_load_textdomain');
 endif;
 
 require_once SPECTRE_ICONS_PATH . '/includes/class-spectre-icons-svg-sanitizer.php';
@@ -49,7 +50,7 @@ require_once SPECTRE_ICONS_PATH . '/includes/elementor/class-spectre-icons-eleme
 require_once SPECTRE_ICONS_PATH . '/includes/elementor/class-spectre-icons-elementor-library-manager.php';
 require_once SPECTRE_ICONS_PATH . '/includes/elementor/icon-libraries.php';
 
-if ( ! function_exists( 'spectre_icons_get_asset_version' ) ) :
+if (! function_exists('spectre_icons_get_asset_version')) :
 	/**
 	 * Generate a cache-busting version string for plugin assets.
 	 *
@@ -57,15 +58,15 @@ if ( ! function_exists( 'spectre_icons_get_asset_version' ) ) :
 	 *
 	 * @return string
 	 */
-	function spectre_icons_get_asset_version( $relative_path ) {
+	function spectre_icons_get_asset_version($relative_path) {
 		$default_version = SPECTRE_ICONS_VERSION;
-		$path            = trailingslashit( SPECTRE_ICONS_PATH ) . ltrim( $relative_path, '/\\' );
+		$path            = trailingslashit(SPECTRE_ICONS_PATH) . ltrim($relative_path, '/\\');
 
-		if ( ! file_exists( $path ) ) {
+		if (! file_exists($path)) {
 			return $default_version;
 		}
 
-		return (string) filemtime( $path );
+		return (string) filemtime($path);
 	}
 endif;
 
